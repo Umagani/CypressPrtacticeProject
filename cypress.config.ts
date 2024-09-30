40521421b879d9a8ca26872007b100ac47e65c5f
umagani <umadevi.gani@gmail.com>
2024-09-30 15:49:43 +0530

    commit chnages node events

--- cypress.config.ts
+++ cypress.config.ts
@@ -30,6 +30,10 @@ export default defineConfig({
   // },
 
   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
     //baseUrl: 'https://rahulshettyacademy.com/seleniumPractise/#/',
     baseUrl: 'https://rahulshettyacademy.com/client/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
